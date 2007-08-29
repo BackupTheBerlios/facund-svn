4fe6a9534edbbfd320acaadf40d19dddea623e8f
zxombie <zxombie@a2d54249-a238-0410-9ff0-d45cad593f5a>
2007-08-29 03:05:24 +0000

    Add tests to check sending messages over the socket works

--- trunk/lib/test/test_facund_connection.c
+++ trunk/lib/test/test_facund_connection.c
@@ -29,7 +29,13 @@
 
 #include <facund_connection.h>
 
-/* Tests for a facund_connection server */
+#include <sys/stat.h>
+
+#include <string.h>
+
+/*
+ * Tests for a facund_connection server
+ */
 START_TEST(pkg_freebsd_connection_server_null)
 {
 	/* Check a NULL argument returns NULL */
@@ -37,7 +43,25 @@ START_TEST(pkg_freebsd_connection_server_null)
 }
 END_TEST
 
-/* Tests for a facund_connection client */
+START_TEST(pkg_freebsd_connection_server_good)
+{
+	struct facund_conn *conn;
+	struct stat sb;
+
+	fail_unless((conn = facund_connect_server("/tmp/facund_test")) != NULL,
+	    NULL);
+	fail_unless(lstat("/tmp/facund_test", &sb) == 0, NULL);
+	fail_unless(S_ISSOCK(sb.st_mode), NULL);
+	facund_cleanup(conn);
+
+	/* Check the socket is cleaned up */
+	fail_unless(lstat("/tmp/facund_test", &sb) == -1, NULL);
+}
+END_TEST
+
+/*
+ * Tests for a facund_connection client
+ */
 START_TEST(pkg_freebsd_connection_client_null)
 {
 	/* Check a NULL argument returns NULL */
@@ -48,7 +72,38 @@ END_TEST
 START_TEST(pkg_freebsd_connection_client_nonexistant)
 {
 	/* Check a non-existant socket returns NULL */
-	fail_unless(facund_connect_server("/nonexistant") == NULL);
+	fail_unless(facund_connect_client("/nonexistant") == NULL, NULL);
+}
+END_TEST
+
+/*
+ * Tests for both working together
+ */
+START_TEST(pkg_freebsd_connection_cs_connects)
+{
+	char buf[5];
+
+	struct facund_conn *conn_s, *conn_c;
+	fail_unless((conn_s = facund_connect_server("/tmp/facund_test"))
+	    != NULL, NULL);
+	fail_unless((conn_c = facund_connect_client("/tmp/facund_test"))
+	    != NULL, NULL);
+	fail_unless(facund_accept(conn_s) == 0, NULL);
+
+	/* Test sending Client -> Server works */
+	fail_unless(facund_send(conn_c, "test", 4) == 4, NULL);
+	fail_unless(facund_recv(conn_s, buf, 4) == 4, NULL);
+	buf[4] = '\0';
+	fail_unless(strcmp(buf, "test") == 0, NULL);
+
+	/* Test sending Server -> Client works */
+	fail_unless(facund_send(conn_s, "mesg", 4) == 4, NULL);
+	fail_unless(facund_recv(conn_c, buf, 4) == 4, NULL);
+	buf[4] = '\0';
+	fail_unless(strcmp(buf, "mesg") == 0, NULL);
+
+	facund_close(conn_c);
+	facund_cleanup(conn_s);
 }
 END_TEST
 
@@ -62,11 +117,16 @@ facund_connection_suite()
 
 	tc = tcase_create("server");
 	tcase_add_test(tc, pkg_freebsd_connection_server_null);
+	tcase_add_test(tc, pkg_freebsd_connection_server_good);
 	suite_add_tcase(s, tc);
 
-	tc = tcase_create("server");
+	tc = tcase_create("client");
 	tcase_add_test(tc, pkg_freebsd_connection_client_null);
 	tcase_add_test(tc, pkg_freebsd_connection_client_nonexistant);
+	suite_add_tcase(s, tc);
+
+	tc = tcase_create("client server");
+	tcase_add_test(tc, pkg_freebsd_connection_cs_connects);
 	suite_add_tcase(s, tc);
 
 	return s;
